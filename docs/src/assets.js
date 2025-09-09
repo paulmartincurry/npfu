211090fd97634e593cc62350cde0141728f0adc4
paulmartincurry <paulmartincurry@gmail.com>
2025-09-08 21:31:58 -0700

    feat: add sprite sheet definitions to assets.js for player, enemies, boss, and hazards

--- docs/src/assets.js
+++ docs/src/assets.js
@@ -1,16 +1,33 @@
-// Drop sprites into /docs/img and register here when ready.
 const sheetDefs = {
-  // example: player: { url: "./img/player.png", w: 48, h: 48, frames: 6 }
+  player_punk:  { url: "./img/player_punk.png",  w: 48, h: 48, frames: 6, rate: 90 },
+  bonehead:     { url: "./img/bonehead.png",     w: 48, h: 48, frames: 4, rate: 110 },
+  cop_riot:     { url: "./img/cop_riot.png",     w: 48, h: 48, frames: 4, rate: 110 },
+  flag_bearer:  { url: "./img/flag_bearer.png",  w: 56, h: 56, frames: 4, rate: 120 },
+  boss_commander: { url: "./img/boss_commander.png", w: 64, h: 64, frames: 6, rate: 110 },
+  horse:        { url: "./img/horse.png",        w: 72, h: 48, frames: 4, rate: 120 },
 };
 
 export class Assets {
-  constructor(){ this.images=new Map(); this.meta=sheetDefs; }
+  constructor(){
+    this.images = new Map();
+    this.meta = sheetDefs;
+  }
   async loadAll(){
-    const entries=Object.entries(this.meta);
-    await Promise.all(entries.map(async([key,def])=>{
-      const img=new Image(); img.src=def.url; await img.decode();
-      this.images.set(key,img);
+    const entries = Object.entries(this.meta);
+    await Promise.all(entries.map(async([key, def]) => {
+      try {
+        const img = new Image();
+        img.src = def.url;
+        await img.decode();
+        this.images.set(key, img);
+      } catch (err) {
+        // fail silently; image remains undefined
+      }
     }));
   }
-  getSheet(key){ return { img:this.images.get(key), ...this.meta[key] }; } // {img,w,h,frames}
+  getSheet(key){
+    const meta = this.meta[key];
+    const img = this.images.get(key);
+    return meta ? { img, ...meta } : null;
+  }
 }
